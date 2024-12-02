875de3cef396b2879994a147770d7babd52bc6bf
Guilty03 <innocent04102003@gmail.com>
2024-12-02 17:21:10 +0300

    arithmetic-lab-complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 10);
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod); 
+
+let numbers = [10, 5, 20, 15, 8];
+let max = Math.max(...numbers);
+
+console.log(max); 
